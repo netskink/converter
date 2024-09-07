38b1d7b3560987dbaee0837c8658eb7304ca4c7b
john davis <davis@doom.lan>
2024-09-07 14:50:26 -0400

    Initial Commit

---
+++ converter/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  converter
+//
+//  Created by john davis on 9/7/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ converter/converterApp.swift
@@ -0,0 +1,17 @@
+//
+//  converterApp.swift
+//  converter
+//
+//  Created by john davis on 9/7/24.
+//
+
+import SwiftUI
+
+@main
+struct converterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
