41ab6cb91916a5b17400030084a6e93157fce67d
John F. Davis <davisjf@gmail.com>
2024-09-08 10:36:41 -0400

    wip

--- converter/ContentView.swift
+++ converter/ContentView.swift
@@ -12,13 +12,21 @@ struct ContentView: View {
     @State private var input = 0.0
     
     var output: Double {
-        let inch = input * 25.4
+        // this is wrong. this is the dpi value
+        // for 5.669 mm/pixel
+        let inch = input / 25.4
         return inch
     }
     var body: some View {
         NavigationStack {
             Form {
                 Section(header: Text("mm").textCase(.none)) {
+                    TextField("mm", value: $input, format: .number)
+                }
+                Section(header: Text("inch").textCase(.none)) {
+                    Text(output, format: .number)
+                }
+                Section(header: Text("mm per pixel").textCase(.none)) {
                     TextField("mm", value: $input, format: .number)
                 }
                 Section(header: Text("inch").textCase(.none)) {
