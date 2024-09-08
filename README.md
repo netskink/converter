262f8bd78bbde8c75fa9dadae4afab958a9a607e
john davis <davis@doom.lan>
2024-09-08 09:20:46 -0400

    wip

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # converter
+
+# notes
+
+An ios icon is 1024x1024.  In GIMP, choose Image->Image Properties and it will give the pixels/mm.
+The sample icon I downloaded was 5.669 x 5.669 pixels/mm.  
+
+25.4 mm = 1 inch
+
+5.669 x 5.669 pixels/mm -> 144 dpi
+
+
+* [app icon ideas](https://macosicons.com)
--- converter/ContentView.swift
+++ converter/ContentView.swift
@@ -8,14 +8,24 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var input = 0.0
+    
+    var output: Double {
+        let inch = input * 25.4
+        return inch
+    }
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            Form {
+                Section(header: Text("mm").textCase(.none)) {
+                    TextField("mm", value: $input, format: .number)
+                }
+                Section(header: Text("inch").textCase(.none)) {
+                    Text(output, format: .number)
+                }
+            }
         }
-        .padding()
     }
 }
 
