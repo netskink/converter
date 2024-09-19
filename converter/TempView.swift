3535a8a648bccdc3d2057b95da690dc9a78d5da9
john davis <davis@doom.lan>
2024-09-18 20:27:49 -0400

    wip

--- converter/TempView.swift
+++ converter/TempView.swift
@@ -13,11 +13,16 @@ struct TempView: View {
     
     // Which subscreen is this detail for
     var subscreen: SubScreen.Screens
+    
+    let inputUnits = [0, 5, 7, 10, 15, 20]
+
 
     
     // 23 C = 73.4 F
     
     @State private var input = 0.0
+    @State private var inputUnit = 7
+
     @FocusState private var inputIsFocused: Bool
     
     var output: Double {
@@ -37,6 +42,23 @@ struct TempView: View {
         
         NavigationStack {
             Form {
+                
+                // A better choice for displaying purpse of the widget
+                // Notice even though we typed in mixed case, when
+                // rendered in UI, its all uppercase.
+                Section(header: Text("Choose input units").textCase(.none)) {
+                    Picker("Input units", selection: $inputUnit) {
+                        ForEach(inputUnits, id: \.self) { unit in
+                            Text(unit, format: .percent)
+                        }
+                    }
+                    .pickerStyle(.segmented)
+                }
+
+                
+                
+                
+                
                 Section(header: Text("celsius").textCase(.none)) {
                     TextField("mm", value: $input, format: .number)
                         .keyboardType(.decimalPad)
