7e86579bccd1fb1abb37cd4cea97435c16c19f47
john davis <davis@doom.lan>
2024-09-21 17:44:38 -0400

    length works

--- converter/LengthView.swift
+++ converter/LengthView.swift
@@ -95,79 +95,121 @@ struct LengthView: View {
         NavigationStack {
                         
             Form {
-                //
-                // input metric or imperial
-                //
-                Section(header: Text("Choose input system").textCase(.none)) {
-                    Picker("Input system", selection: $inputSystem) {
-                        ForEach(possibleSystemUnits, id: \.self) { unit in
-                            Text(unit.description)
-                        }
-                    }
-                    .pickerStyle(.segmented)
-                }
-                //
-                // input units:  metric(mm, cm, ...) or imperial(inch, foot, ...)
-                //
-                Section(header: Text("Choose input unit").textCase(.none)) {
-                    Picker("Input unit", selection: $inputUnit) {
-                        if inputSystem == .imperial {
-                            ForEach(possibleImperialLengthUnits, id: \.self) { unit in
-                                Text(unit.description)
+                ZStack {
+                    Color.green.opacity(0.25)
+                        VStack {
+                        //
+                        // input metric or imperial
+                        //
+                        Section(header: Text("Choose input system").textCase(.none)) {
+                            Picker("Input system", selection: $inputSystem) {
+                                ForEach(possibleSystemUnits, id: \.self) { unit in
+                                    Text(unit.description)
+                                }
+                            }
+                            .pickerStyle(.segmented)
+//                            .onChange(of: inputSystem) { newValue in
+//                                if inputSystem == .imperial {
+//                                    inputUnit = possibleImperialLengthUnits.first!
+//                                } else {
+//                                    inputUnit = possibleMetricLengthUnits.first!
+//                                }
+//                            }
+                            .onChange(of: inputSystem, initial: true) { oldValue, newValue in
+                                if inputSystem == .imperial {
+                                    inputUnit = possibleImperialLengthUnits.first!
+                                } else {
+                                    inputUnit = possibleMetricLengthUnits.first!
+                                }
                             }
                         }
-                        if inputSystem == .metric {
-                            ForEach(possibleMetricLengthUnits, id: \.self) { unit in
-                                Text(unit.description)
+                        //
+                        // input units:  metric(mm, cm, ...) or imperial(inch, foot, ...)
+                        //
+                        Section(header: Text("Choose input unit").textCase(.none)) {
+                            Picker("Input unit", selection: $inputUnit) {
+                                if inputSystem == .imperial {
+                                    ForEach(possibleImperialLengthUnits, id: \.self) { unit in
+                                        Text(unit.description)
+                                    }
+                                }
+                                if inputSystem == .metric {
+                                    ForEach(possibleMetricLengthUnits, id: \.self) { unit in
+                                        Text(unit.description)
+                                    }
+                                }
                             }
+                            .pickerStyle(.segmented)
                         }
-                    }
-                    .pickerStyle(.segmented)
-                }
 
-                
-                
-                Section(header: Text("mm").textCase(.none)) {
-                    TextField("mm", value: $input, format: .number)
-                        .keyboardType(.decimalPad)
-                        .focused($inputIsFocused)
+                        
+                        
+                        Section(header: Text("Convert From:").textCase(.none)) {
+                            TextField("", value: $input, format: .number)
+                                .keyboardType(.decimalPad)
+                                .focused($inputIsFocused)
 
-                }
-
-                //
-                // output metric or imperial
-                //
-                Section(header: Text("Choose output system").textCase(.none)) {
-                    Picker("Output system", selection: $outputSystem) {
-                        ForEach(possibleSystemUnits, id: \.self) { unit in
-                            Text(unit.description)
                         }
-                    }
-                    .pickerStyle(.segmented)
-                }
-                //
-                // output units:  metric(mm, cm, ...) or imperial(inch, foot, ...)
-                //
-                Section(header: Text("Choose output unit").textCase(.none)) {
-                    Picker("Output unit", selection: $outputUnit) {
-                        if outputSystem == .imperial {
-                            ForEach(possibleImperialLengthUnits, id: \.self) { unit in
-                                Text(unit.description)
+                    } // vstack
+                } // zstack
+
+                ZStack {
+                    Color.blue.opacity(0.25)
+                    VStack {
+                        
+                        
+                        //
+                        // output metric or imperial
+                        //
+                        Section(header: Text("Choose output system").textCase(.none)) {
+                            Picker("Output system", selection: $outputSystem) {
+                                ForEach(possibleSystemUnits, id: \.self) { unit in
+                                    Text(unit.description)
+                                }
                             }
-                        }
-                        if outputSystem == .metric {
-                            ForEach(possibleMetricLengthUnits, id: \.self) { unit in
-                                Text(unit.description)
+                            .pickerStyle(.segmented)
+//                            .onChange(of: outputSystem) { newValue in
+//                                if outputSystem == .imperial {
+//                                    outputUnit = possibleImperialLengthUnits.first!
+//                                } else {
+//                                    outputUnit = possibleMetricLengthUnits.first!
+//                                }
+//                            }
+                            .onChange(of: outputSystem, initial: true) { oldValue, newValue in
+                                if outputSystem == .imperial {
+                                    outputUnit = possibleImperialLengthUnits.first!
+                                } else {
+                                    outputUnit = possibleMetricLengthUnits.first!
+                                }
                             }
+
                         }
-                    }
-                    .pickerStyle(.segmented)
-                }
-
-
-                Section(header: Text("inch").textCase(.none)) {
-                    Text(output, format: .number)
-                }
+                        //
+                        // output units:  metric(mm, cm, ...) or imperial(inch, foot, ...)
+                        //
+                        Section(header: Text("Choose output unit").textCase(.none)) {
+                            Picker("Output unit", selection: $outputUnit) {
+                                if outputSystem == .imperial {
+                                    ForEach(possibleImperialLengthUnits, id: \.self) { unit in
+                                        Text(unit.description)
+                                    }
+                                }
+                                if outputSystem == .metric {
+                                    ForEach(possibleMetricLengthUnits, id: \.self) { unit in
+                                        Text(unit.description)
+                                    }
+                                }
+                            }
+                            .pickerStyle(.segmented)
+                        }
+                        
+                        
+                        Section(header: Text("Convert to:").textCase(.none)) {
+                            Text(output, format: .number)
+                            .multilineTextAlignment(.leading)
+                        }
+                    } // vstack
+                } // zstack
             } // Form
             .navigationTitle("Length")
             // This does nothing
